c8595fff52f7b30af5f690e1f3fadbd97dd7110c
adarrra <adarahv@gmail.com>
2017-11-25 12:32:37 +0300

    send request from sensor-server

--- sensor-server/server.js
+++ sensor-server/server.js
@@ -1,23 +1,25 @@
 const SerialPort = require('serialport');
-const port = new SerialPort('/dev/ttyACM0', {
+const port = new SerialPort('/dev/ttyACM1', {
   baudRate: 9600
 });
 const request = require('superagent');
-
-const TRESHOLD = 1;
+const api = 'http://c56fd690.ngrok.io';
+const TRESHOLD = 51;
 
 port.on('data',  (bytes) => {
     let level = bytes[0].toString();
-    console.log(level);
+    console.log(level); 
+    
     
     if(level > TRESHOLD) {
+        console.log('sending');
         request
-           .post('/api/fire-alarm')
+           .post(api + '/fire-alarm')
            .send({ sensorId: 'SomeId'})
            .set('accept', 'json')
            .end((err, res) => {
-             // Calling the end function will send the request
-           });
+            console.log(res.status);
+          });
     } 
 
 });
