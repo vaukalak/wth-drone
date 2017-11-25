90d765e2da48a8064eeb1dae1bf106e2cf559f27
vaukalak <lahmataja.pa4vara@gmail.com>
2017-11-25 15:03:36 +0300

    linting

--- drone-control-server/drone.js
+++ drone-control-server/drone.js
@@ -1,57 +1,58 @@
-var RollingSpider = require("rolling-spider");
-var temporal = require('temporal');
-var rollingSpider = new RollingSpider();
+const RollingSpider = require('rolling-spider');
+const temporal = require('temporal');
 
-function extinguish(destination) {
-    console.log('Going to extinguish, waiting for connect');
-    rollingSpider.connect(function() {
-        console.log('Connected to drone');
-        rollingSpider.setup(function() {
-            console.log('Connected to drone', rollingSpider.name);
-            temporal.queue([{
-                    delay: 1000,
-                    task: function() {
-                        console.log('takeoff');
-                        rollingSpider.flatTrim();
-                        rollingSpider.takeOff();
-                        rollingSpider.flatTrim();
-                    }
-                },
-            
-                {
-                    delay: 2000,
-                    task: function() {
-                        console.log('forward');
-                        rollingSpider.forward({
-                            steps: 14
-                        });
-                    }
-                },
-                {
-                    delay: 4000,
-                    task: land
-                }
-            ]);
-        });
-    });
-}
+const rollingSpider = new RollingSpider();
 
-function emergancy() {
-    rollingSpider.emergancy()
+function landCb() {
+  console.log('land callback called');
+  temporal.clear();
 }
 
 function land() {
-    console.log('land');
-    rollingSpider.land(landCb);
+  console.log('land');
+  rollingSpider.land(landCb);
 }
- 
-function landCb() {
-    console.log('land callback called');
-    temporal.clear();
+
+function extinguish() {
+  console.log('Going to extinguish, waiting for connect');
+  rollingSpider.connect(() => {
+    console.log('Connected to drone');
+    rollingSpider.setup(() => {
+      console.log('Connected to drone', rollingSpider.name);
+      temporal.queue([{
+        delay: 1000,
+        task() {
+          console.log('takeoff');
+          rollingSpider.flatTrim();
+          rollingSpider.takeOff();
+          rollingSpider.flatTrim();
+        },
+      },
+
+      {
+        delay: 2000,
+        task() {
+          console.log('forward');
+          rollingSpider.forward({
+            steps: 14,
+          });
+        },
+      },
+      {
+        delay: 4000,
+        task: land,
+      },
+      ]);
+    });
+  });
+}
+
+function emergancy() {
+  rollingSpider.emergancy();
 }
 
 module.exports = {
-    extinguish,
-    emergancy,
-    land
+  extinguish,
+  emergancy,
+  land,
 };
--- drone-control-server/server.js
+++ drone-control-server/server.js
@@ -1,41 +1,42 @@
 const express = require('express');
 const drone = require('./drone');
 const bodyParser = require('body-parser');
+
 const app = express();
 const stdin = process.openStdin();
 
-stdin.addListener("data", function(d) {
-    d = d.toString().trim();
-    console.log(typeof d);
-    if (d == 'exit') {
-        console.log('BYE');
-        process.exit()
-    } 
-    if (d == 'e') {
-        console.log('TURN OFF');
-        drone.emergancy()
-    }
-    if (d == 'land') {
-        console.log('LAND');
-        drone.land();
-    }
-    console.log("you entered: " + d);
-  });
+stdin.addListener('data', (initialD) => {
+  const d = initialD.toString().trim();
+  console.log(typeof d);
+  if (d === 'exit') {
+    console.log('BYE');
+    process.exit();
+  }
+  if (d === 'e') {
+    console.log('TURN OFF');
+    drone.emergancy();
+  }
+  if (d === 'land') {
+    console.log('LAND');
+    drone.land();
+  }
+  console.log(`you entered: ${d}`);
+});
 
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false,
 }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/fire-alarm', function(req, res) {
-    const sensorId = req.body.sensorId;
+app.post('/fire-alarm', (req, res) => {
+  const { sensorId } = req.body;
 
-    const message = `Fire in the hole at sensor ${sensorId}!`;
-    console.log(message);
+  const message = `Fire in the hole at sensor ${sensorId}!`;
+  console.log(message);
 
-    drone.extinguish(sensorId);
+  drone.extinguish(sensorId);
 
-    res.send(message);
+  res.send(message);
 });
 
 app.listen(3000);
