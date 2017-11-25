213f2d86c1d16cb033b5f5076d286dfc51c9ef86
adarrra <adarahv@gmail.com>
2017-11-25 11:51:23 +0300

    chore drone server code

--- drone-control-server/index.js
+++ drone-control-server/index.js
@@ -1,45 +1,41 @@
 var RollingSpider = require("rolling-spider");
 var temporal = require('temporal');
 var rollingSpider = new RollingSpider();
-console.log('start')
-rollingSpider.connect(function () {
-    console.log('connected')
-    rollingSpider.setup(function () {
-        console.log('takeoff')
-        //rollingSpider.takeOff();
-        // console.log('flatTrim')
-        // rollingSpider.flatTrim();
-        // rollingSpider.startPing();
-        // rollingSpider.flatTrim();
 
-        // temporal.queue([
-        //     {
-        //         delay: 5000,
-        //         task: function () {
-        //             console.log('takeoff')
-        //             rollingSpider.takeOff();
-        //             rollingSpider.flatTrim();
-        //         }
-        //     },
-        //     {
-        //         delay: 3000,
-        //         task: function () {
-        //             rollingSpider.forward();
-        //         }
-        //     },
-        //     {
-        //         delay: 5000,
-        //         task: function () {
-        //             rollingSpider.land();
-        //         }
-        //     },
-        //     {
-        //         delay: 5000,
-        //         task: function () {
-        //             temporal.clear();
-        //             process.exit(0);
-        //         }
-        //     }
-        // ]);
+console.log('drone control server started')
+
+rollingSpider.connect(function () {
+    console.log('initial connect')
+    rollingSpider.setup(function () {
+        console.log('Connected to drone', rollingSpider.name);
+        rollingSpider.flatTrim();
+        rollingSpider.takeOff();
+        rollingSpider.flatTrim();
+        console.log('takeoff')
+
+        temporal.queue([
+
+            {
+                delay: 2000,
+                task: function () {
+                    console.log('forward');
+                    rollingSpider.forward({steps: 4});
+                }
+            },
+            {
+                delay: 4000,
+                task: function () {
+                    console.log('land');
+                    rollingSpider.land();
+                }
+            },
+            {
+                delay: 5000,
+                task: function () {
+                    temporal.clear();
+                    process.exit(0);
+                }
+            }
+        ]);
     });
 });
