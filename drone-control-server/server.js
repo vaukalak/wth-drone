2a67d5c07a2c1f894efe55a961f9cd1f867f89c9
Pavel Bely <pavel.biely@yandex.by>
2017-11-25 12:55:52 +0300

    Add drone extinguisher

--- drone-control-server/server.js
+++ drone-control-server/server.js
@@ -1,16 +1,23 @@
-var express = require('express');
+const express = require('express');
+const drone = require('./drone');
 const bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json())
 
-app.post('/fire-alarm', function (req, res) {
-  const sensorId = req.body.sensorId;
-  
-  const message = `Fire in the hole at sensor ${sensorId}!`;
-  console.log(message);
-  
-  res.send(message);
+app.post('/fire-alarm', function(req, res) {
+    const sensorId = req.body.sensorId;
+
+    const message = `Fire in the hole at sensor ${sensorId}!`;
+    console.log(message);
+
+    drone.extinguish(sensorId);
+
+    res.send(message);
 });
 
 app.listen(3000);
+
+console.log('drone control server started');
